83d424d4c804d10afe15e08a8fcb18ff7cdae3ad
C1olmB185852 <dan.ielestebanmat@gmail.com>
2024-03-05 18:12:13 -0500

    array constructor array

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ let arrayConElementos = [1, 2, 3];
 // Array con elementos de diferentes tipos de datos:
 
 let arrayConElementosDiferentes = [1, 'Daniel', false];
+
+// Array utilizando el constructor Array():
+
+let arrayConConstructorArray = new Array(5, 'cuatro', true);
