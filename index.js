18c45546d090ab0f5e8a8bbae2e2eac06b8ffb30
C1olmB185852 <dan.ielestebanmat@gmail.com>
2024-03-05 18:16:50 -0500

    array con secuencia numerica

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ let arrayConElementosDiferentes = [1, 'Daniel', false];
 // Array utilizando el constructor Array():
 
 let arrayConConstructorArray = new Array(5, 'cuatro', true);
+
+// Array con una secuencia numérica usando el método Array.from( )
+
+let secuenciaNumerica = Array.from({length: 10}, (_, index) => index);
+console.log(secuenciaNumerica); 
