16fd867f277f15c4ad5b5439948dca05989906ec
C1olmB185852 <dan.ielestebanmat@gmail.com>
2024-03-05 18:07:58 -0500

    array con elementos diferentes

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ let arrayVacio = [ ];
 // Array con elementos
 
 let arrayConElementos = [1, 2, 3];
+
+// Array con elementos de diferentes tipos de datos:
+
+let arrayConElementosDiferentes = [1, 'Daniel', false];
